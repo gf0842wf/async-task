2c53d1c73872b9609ac7a862682ecee10821bed8
gf0842wf <gf0842wf@gmail.com>
2014-09-07 11:47:29 +0800

    m
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- task.py
+++ task.py
@@ -19,7 +19,7 @@ class Task(object):
         self.conn = redis.StrictRedis(host=host, port=port, db=db)
 
     def req_msg(self, msg, block=False):
-        """@param tap: 消息队列标识(域.具体名)
+        """请求消息
            : 把请求的消息放在list task.mq 里
            : 把响应的消息放在list task.tap 里
         """
@@ -31,7 +31,8 @@ class Task(object):
             return self.conn.brpop(tap)
         
     def process_msg(self, block=True, timeout=0):
-        """@param timeout: 当block=True时才有用
+        """回复消息
+           @param timeout: 当block=True时才有用
            : 处理消息可以是外部别的语言写的程序,可以开多个消费者来处理消息
         """
         if block:
