a026f1658f3a38c231fab2f8a5b6af77480a11b0
gf0842wf <gf0842wf@gmail.com>
2014-09-07 10:02:14 +0800

    m
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- task.py
+++ task.py
@@ -20,24 +20,27 @@ class Task(object):
 
     def req_msg(self, msg, block=False):
         """@param tap: 消息队列标识(域.具体名)
+           : 把请求的消息放在list task.mq 里
+           : 把响应的消息放在list task.tap 里
         """
         tap = "task."+uuid.uuid1().hex
         data = pickle.dumps((tap, msg))
         
-        self.conn.lpush("mq.task", data)
+        self.conn.lpush("task.mq", data)
         if block:
             return self.conn.brpop(tap)
         
     def process_msg(self, block=True, timeout=0):
         """@param timeout: 当block=True时才有用
+           : 处理消息可以是外部别的语言写的程序,可以开多个消费者来处理消息
         """
         if block:
-            _data = self.conn.brpop("mq.task", timeout)
+            _data = self.conn.brpop("task.mq", timeout)
+            if not _data: raise TaskError("timeout")
+            return
         else:
-            _data = self.conn.rpop("mq.task")
-        
-        if not _data and block:
-            raise TaskError("timeout")
+            _data = self.conn.rpop("task.mq")
+            return
         
         _, data = _data
         
