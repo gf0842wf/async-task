181e008ea5a4a8046653a3fdd482a3b5e7e7233f
gf0842wf <gf0842wf@gmail.com>
2014-09-09 10:21:45 +0800

    m
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- test_client.py
+++ test_client.py
@@ -3,12 +3,10 @@
 # 先执行 python task.py --mq=task.mq --green=1
 
 from task import Task
-import gevent
-    
-t = Task(mq="task.mq", use_greenlets=True)
-
 import time
-t1 = time.time()
-print t.block_add(60, 2, 4)
+    
+tk = Task(mq="task.mq", use_greenlets=True)
+
+tk = time.time()
+print tk.block_add(60, 2, 4)
 print time.time() - t1
-gevent.wait()
